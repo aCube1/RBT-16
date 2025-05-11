ea2826c793b38bb47bb10276ab7c15442a7d37e2
aCube <player559001@gmail.com>
2025-05-11 15:46:55 -0300

    Add Sol2 library for Lua support.

--- cmake/libraries.cmake
+++ cmake/libraries.cmake
@@ -5,7 +5,17 @@ set(_LIBS_DIR ${CMAKE_SOURCE_DIR}/ext)
 function(setup_libraries target)
 	target_include_directories(
 		${target} PRIVATE
-			${_LIBS_DIR}
+			"${_LIBS_DIR}"
+	)
+
+	set(SOL2_LUA_VERSION "5.4.4")
+	set(SOL2_BUILD_LUA TRUE)
+	option(SOL2_ENABLE_INSTALL "" OFF)
+	add_subdirectory("${_LIBS_DIR}/sol2")
+
+	target_link_libraries(
+		${target} PRIVATE
+			sol2::sol2
 	)
 
 	if(UNIX)
