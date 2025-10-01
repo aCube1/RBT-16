ee63428b825a83272803884375ab4946bc94a167
aCube <player559001@gmail.com>
2025-10-01 01:05:56 -0300

    Add "check" build step for ZLS integration.

--- build.zig
+++ build.zig
@@ -4,13 +4,15 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
     const exe = b.addExecutable(.{
         .name = "rbt16-emu",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
+        .root_module = exe_mod,
     });
 
     b.installArtifact(exe);
@@ -30,4 +32,11 @@ pub fn build(b: *std.Build) void {
     const run_tests = b.addRunArtifact(tests);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_tests.step);
+
+    const exe_check = b.addExecutable(.{
+        .name = "testing",
+        .root_module = exe_mod,
+    });
+    const check = b.step("check", "Check for errors");
+    check.dependOn(&exe_check.step);
 }
